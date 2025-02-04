88e97259e25d9063214e26623c4e9a04f953e327
Joris RICARD <ricardjoris@gmail.com>
2025-02-04 20:27:50 +0100

    Initial commit: Setup project structure with Spring Boot backend and React frontend

---
+++ backend/warehousepro-backend/src/main/java/com/warehousepro/warehousepro_backend/WarehouseproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.warehousepro.warehousepro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseproBackendApplication.class, args);
+	}
+
+}
---
+++ backend/warehousepro-backend/src/test/java/com/warehousepro/warehousepro_backend/WarehouseproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehousepro.warehousepro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
